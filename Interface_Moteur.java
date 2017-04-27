d4670a37ae258c5b77958e9c258fbfcb9322b6c2
TheSpyGeek <thespygeek@localhost.localdomain>
2017-04-27 11:23:34 +0200

    ajout de l'interface pour implementer Moteur.java

---
+++ Interface_Moteur.java
@@ -0,0 +1,16 @@
+public interface Interface_Moteur {
+
+
+	public void eat(int i, int j);
+
+	public void save(String file);
+
+	public void undo();
+
+	public load(String file);
+
+	public void print_text();
+
+	public void update_graphic();
+
+}

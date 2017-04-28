58bc4d4533b0f6e6101c180beafe0d38b03da2ec
Sanory <t.hibs@hotmail.fr>
2017-04-28 12:05:23 +0200

    creation de la scene Menu, ajout du logo Waffle 1.0

--- MenuInterface.java
+++ MenuInterface.java
@@ -3,25 +3,57 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class MenuInterface extends Application{
+    
+    private final int winWidth = 1080;
+    private final int winHeight = 720;
 
     @Override
     public void start(Stage primaryStage) {
-    	Button newGame = createButton("Nouvelle partie", "0");
-    	Button loadGame = createButton("Continuer la partie", "1");
-        loadGame.setDisable(true); // permet de désactiver un boutton (utile pour le boutton "Continuer" lorsqu'aucue partie n'est savegardée)
         
-        GridPane root = new GridPane();
-        root.getChildren().add(newGame);
-        root.getChildren().add(loadGame);
-
- 		Scene scene = new Scene(root, 300, 250);
+        //Buttons
+    	Button newGame = createButton("  Nouvelle partie  ", "0");
+    	Button loadGame = createButton("Continuer la partie", "1");
+        Button exitGame = createButton("  Quitter le jeu   ", "2");
+        loadGame.setDisable(true); // permet de désactiver un boutton (utile pour le boutton "Continuer" lorsqu'aucue partie n'est savegardée)
+        newGame.setMaxHeight(Double.MAX_VALUE);
+        loadGame.setMaxHeight(Double.MAX_VALUE);
+        exitGame.setMaxHeight(Double.MAX_VALUE);
+        newGame.setMinWidth(300);
+        loadGame.setMinWidth(300);
+        exitGame.setMinWidth(300);
+        newGame.setStyle("-fx-font-size: 20pt;");
+        loadGame.setStyle("-fx-font-size: 20pt;");
+        exitGame.setStyle("-fx-font-size: 20pt;");
+      
+        
+        //Grid
+        GridPane menu = new GridPane();
+        menu.setHgap(5);
+        menu.setVgap(5);
+        menu.add(newGame,75,30);
+        menu.add(loadGame,75,35);
+        menu.add(exitGame,75,40);
+        Image imgTitle = new Image("ressources/waffle_title.png");
+        ImageView viewTitle = new ImageView(imgTitle);
+        menu.add(viewTitle, 55, 10);
+        GridPane.setColumnSpan(viewTitle, 200);
+        
+ 	Scene scene = new Scene(menu, 300, 250);
 
         primaryStage.setTitle("Waffle 1.0");
+        primaryStage.setWidth(winWidth);
+        primaryStage.setHeight(winHeight);
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("ressources/waffle.png"));
         primaryStage.setScene(scene);
+        
         primaryStage.show();
     }
 
@@ -48,6 +80,9 @@ public class MenuInterface extends Application{
     		case "1":
     			System.out.println("Chargement de la partie en cours");
     			break;
+                case "2":
+                        System.exit(0);
+                        break;
     	}
         }
     };
